b65e255214299116b3974cf58ce0b6b0c3eb9a76
jawkneemai <john@jtkmai.com>
2020-08-05 18:05:51 -0700

    currently scrapes all player <tr> elements, but only has access to each element in a loop. want to do a BeautifulSoup find_all() with queries to exclude <tr> elements with class=thead

--- pfrScrape.py
+++ pfrScrape.py
@@ -12,27 +12,64 @@ pp = pprint.PrettyPrinter(indent=4)
 
 # URLs: Queries list every player that has the respective stats. 
 # Format: 100 players listed in a table element every page, click next button to get the next 100 players
-rush_url = 'https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min=1970&year_max=2019&season_start=1&season_end=-1&pos%5B%5D=QB&pos%5B%5D=WR&pos%5B%5D=RB&pos%5B%5D=TE&is_starter=E&game_type=R&game_num_min=0&game_num_max=99&week_num_min=0&week_num_max=99&c5val=1.0&order_by=rush_yds'
-rec_url = 'https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min=1970&year_max=2019&season_start=1&season_end=-1&pos%5B%5D=QB&pos%5B%5D=WR&pos%5B%5D=RB&pos%5B%5D=TE&is_starter=E&game_type=R&game_num_min=0&game_num_max=99&week_num_min=0&week_num_max=99&c5val=1.0&order_by=rec_yds'
+rush_url = 'https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min=1970&year_max=2019&season_start=1&season_end=-1&pos%5B%5D=QB&pos%5B%5D=WR&pos%5B%5D=RB&pos%5B%5D=TE&is_starter=E&game_type=R&game_num_min=0&game_num_max=99&week_num_min=0&week_num_max=99&c5val=1.0&order_by=rush_att'
+rec_url = 'https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min=1970&year_max=2019&season_start=1&season_end=-1&pos%5B%5D=QB&pos%5B%5D=WR&pos%5B%5D=RB&pos%5B%5D=TE&is_starter=E&game_type=R&game_num_min=0&game_num_max=99&week_num_min=0&week_num_max=99&c5val=1.0&order_by=rec'
 pass_url = 'https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min=1970&year_max=2019&season_start=1&season_end=-1&pos%5B%5D=QB&pos%5B%5D=WR&pos%5B%5D=RB&pos%5B%5D=TE&is_starter=E&game_type=R&game_num_min=0&game_num_max=99&week_num_min=0&week_num_max=99&c5val=1.0&order_by=pass_yds'
 
 # Other Variables
-rush_page = requests.get(rush_url)
-rec_page = requests.get(rec_url)
-pass_page = requests.get(pass_url)
-rush_soup = BeautifulSoup(rush_page.content, 'html.parser')
-rec_soup = BeautifulSoup(rec_page.content, 'html.parser')
-pass_soup = BeautifulSoup(pass_page.content, 'html.parser')
+next_page_url = '&offset=' 
+# Query to move to specified rank of the stat. (PFR only displays 100 players at a time)
+# Add an integer after '=' to jump to specific rank.
+# i.e.: Rushing: 41679 players with rushing attempts in any game
 
-# Getting all <tr> elements with player data
-rush_tr_elements = rush_soup.find_all('tr')
-for i in rush_tr_elements:
-	print(i, end='\n'*2)
+# Scraping Rushing Stats of ALL Players
 
-rec_tr_elements = rec_soup.find_all('tr')
-for i in rec_tr_elements:
-	print(i, end='\n'*2)
+isDone = 0 # Boolean for checking if end of stats table on PFR
+counter = 0
+#rush_url = rush_url + next_page_url + '200'
+try:
+	rush_page = requests.get(rush_url)
+	rush_soup = BeautifulSoup(rush_page.content, 'html.parser')
+	rush_tbody = rush_soup.find('tbody')
+	rush_tr_elements = rush_tbody.find_all('tr')
+except AttributeError:
+	print('Error creating or navigating Soup object')
 
-pass_tr_elements = pass_soup.find_all('tr')
-for i in pass_tr_elements:
-	print(i, end='\n'*2)
+for tr in rush_tr_elements:
+	if tr.has_attr('class'):
+		print(tr.prettify())
+	else:
+		#print(tr.prettify())
+		counter += 1
+		print(counter)
+
+while isDone == 0:
+	counter += 1
+	print(counter)
+	isDone = 1
+	#rush_page = requests.get(rush_url)
+	#rush_soup = BeautifulSoup(rush_page.content, 'html.parser')
+
+#rush_tr_elements = rush_soup.find_all('tr')
+#for i in rush_tr_elements:
+#	print(i, end='\n'*2)
+
+
+
+
+#rec_page = requests.get(rec_url)
+#pass_page = requests.get(pass_url)
+#rec_soup = BeautifulSoup(rec_page.content, 'html.parser')
+#pass_soup = BeautifulSoup(pass_page.content, 'html.parser')
+
+#rec_tr_elements = rec_soup.find_all('tr')
+#for i in rec_tr_elements:
+#	print(i, end='\n'*2)
+
+#pass_tr_elements = pass_soup.find_all('tr')
+#for i in pass_tr_elements:
+#	print(i, end='\n'*2)
+
+
+
+### Functions ###
